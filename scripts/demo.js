4708c7b7cbea56b706bfc9f6f336fec3bd2607ae
chenyu <535509852@qq.com>
2019-05-08 02:30:32 +0800

    bg

---
+++ scripts/demo.js
@@ -0,0 +1,80 @@
+let swRegistration;
+const action = location.search.replace("?action=", "");
+switch (action) {
+  case "eTicket":
+    alert("电子票：xxx");
+    break;
+  case "open":
+    alert("点击了打开页面");
+    break;
+  default:
+}
+
+// 分享按钮事件
+document.querySelector("#share").addEventListener("click", () => {
+  if (navigator.share === undefined) {
+    alert("不支持原生分享！");
+    return;
+  }
+  try {
+    navigator.share({
+      title: "旅游网",
+      text: "这是分享内容",
+      url: "https://mydearest.cn"
+    });
+  } catch (err) {
+    alert(err);
+  }
+});
+
+const body = document.querySelector("body");
+const footer = document.querySelector(".offline");
+if (!navigator.onLine) {
+  body.classList.add("offline");
+  footer.style.bottom = "0";
+}
+
+window.ononline = () => {
+  body.classList.remove("offline");
+  footer.style.bottom = "-51px";
+};
+
+window.onoffline = () => {
+  body.classList.add("offline");
+  footer.style.bottom = "0";
+};
+
+// 注册ServiceWorker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./service-worker.js").then(registration => {
+    console.log("Service Worker Registered");
+    swRegistration = registration;
+  });
+
+  // 发送消息按钮事件
+  document.querySelector("#sendMsg").addEventListener("click", () => {
+    if (swRegistration) {
+      Notification.requestPermission(result => {
+        if (result === "granted") {
+          swRegistration.showNotification("驴妈妈旅游网", {
+            body: "出票成功！",
+            actions: [
+              {
+                action: "eTicket",
+                title: "查看电子票"
+              },
+              {
+                action: "open",
+                title: "打开站点"
+              }
+            ],
+            icon: "./images/logo.png",
+            vibrate: [200, 100, 200, 100, 200, 100, 200]
+          });
+        } else {
+          alert(result);
+        }
+      });
+    }
+  });
+}
---
+++ service-worker.js
@@ -0,0 +1,63 @@
+const cacheName = "pwa_1.0.0";
+const filesToCache = ["./index.html", "./scripts/demo.js", "./styles/demo.css"];
+
+// 安装
+self.addEventListener("install", e => {
+  console.log("[ServiceWorker] Install");
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log("[ServiceWorker] Caching app shell");
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+// 激活
+self.addEventListener("activate", e => {
+  console.log("[ServiceWorker] Activate");
+  e.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== cacheName) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  /*
+   * Fixes a corner case in which the app wasn't returning the latest data.
+   * You can reproduce the corner case by commenting out the line below and
+   * then doing the following steps: 1) load app for first time so that the
+   * initial New York City data is shown 2) press the refresh button on the
+   * app 3) go offline 4) reload the app. You expect to see the newer NYC
+   * data, but you actually see the initial data. This happens because the
+   * service worker is not yet activated. The code below essentially lets
+   * you activate the service worker faster.
+   */
+  return self.clients.claim();
+});
+
+// 捕获请求
+self.addEventListener("fetch", e => {
+  console.log("[Service Worker] Fetch", e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
+
+// 点击通知事件
+self.addEventListener("notificationclick", e => {
+  console.log("[Service Worker] Notification click Received.");
+  e.notification.close();
+  if (e.action === "eTicket") {
+    e.waitUntil(clients.openWindow("./index.html?action=eTicket"));
+  } else if (e.action === "open") {
+    e.waitUntil(clients.openWindow("./index.html?action=open"));
+  } else {
+    e.waitUntil(clients.openWindow("./index.html"));
+  }
+});
